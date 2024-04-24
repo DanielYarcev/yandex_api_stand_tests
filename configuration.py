0ab6917a2c9b4831f9aca765b9e25876eea8a185
Daniel <wolfyarcev@gmail.com>
2024-04-25 00:46:16 +0300

    my first commit

---
+++ configuration.py
@@ -0,0 +1,13 @@
+
+URL_SERVICE = "https://94aca3ca-cb9e-454a-929b-9864c9e33f3c.serverhub.praktikum-services.ru"
+
+
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
